4561e1f9972b15b21b1187a1166ba2791e20e9f4
Oluwatomisin Gbotolorun <tosingbotolorun@yahoo.co.uk>
2018-12-11 11:34:25 +0100

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  expand(){
+    document.getElementById('dropdown-btn').classList.toggle('tap');
+  }
   title = 'foodee';
 }
